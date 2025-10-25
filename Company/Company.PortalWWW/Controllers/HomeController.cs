b175d92c2775f2d9d321f332ee4233b3992e126d
jtjaskulski@outlook.com <jtjaskulski@outlook.com>
2025-10-25 14:58:44 +0200

    Dodane About I Contact

--- Company/Company.PortalWWW/Controllers/HomeController.cs
+++ Company/Company.PortalWWW/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace Company.PortalWWW.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
