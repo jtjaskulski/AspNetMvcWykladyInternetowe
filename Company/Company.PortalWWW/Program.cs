2ddb5115bfd31b8b8e703471563043cd50586551
jtjaskulski@outlook.com <jtjaskulski@outlook.com>
2025-11-08 14:42:17 +0100

    poprawki w appsettingach i zainicjalizowanie bazy danych w PortalWWW

--- Company/Company.PortalWWW/Program.cs
+++ Company/Company.PortalWWW/Program.cs
@@ -1,3 +1,6 @@
+using Company.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.PortalWWW
 {
     public class Program
@@ -5,6 +8,8 @@ namespace Company.PortalWWW
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<CompanyContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CompanyContext") ?? throw new InvalidOperationException("Connection string 'CompanyContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
