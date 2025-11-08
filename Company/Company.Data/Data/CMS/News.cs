6eed45a5e9a46835b0a3542a4cda75f135c55d90
jtjaskulski@outlook.com <jtjaskulski@outlook.com>
2025-11-08 14:39:45 +0100

    zmiany i migracje do bazy w projekcie data

--- Company/Company.Data/Data/CMS/News.cs
+++ Company/Company.Data/Data/CMS/News.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Company.Intranet.Models.CMS
+namespace Company.Data.Data.CMS
 {
     public class News
     {
--- Company/Company.Data/Data/CompanyContext.cs
+++ Company/Company.Data/Data/CompanyContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Company.Data.Data;
+﻿using Company.Data.Data.CMS;
+using Company.Data.Data.Shop;
+using Microsoft.EntityFrameworkCore;
 
-public class CompanyContext
+namespace Company.Data.Data;
+
+public class CompanyContext : DbContext
 {
-    
+    public CompanyContext (DbContextOptions<CompanyContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Page> Page { get; set; } = default!;
+    public DbSet<News> News { get; set; } = default!;
+    public DbSet<TypeOfProduct> TypeOfProduct { get; set; } = default!;
+    public DbSet<Product> Product { get; set; } = default!;
 }
