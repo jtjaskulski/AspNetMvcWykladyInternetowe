a6ed53b810c8c60eaa89003e0a00513deface5bc
jtjaskulski@outlook.com <jtjaskulski@outlook.com>
2025-11-16 16:07:11 +0100

    Szczegółły produktu front end

--- Company/Company.PortalWWW/Controllers/ShopController.cs
+++ Company/Company.PortalWWW/Controllers/ShopController.cs
@@ -18,18 +18,25 @@ namespace Company.PortalWWW.Controllers
         public async Task<IActionResult> Index(int? id)
         {
             ViewBag.TypeOfProducts = await _context.TypeOfProduct.ToListAsync();
+            ViewBag.ModelNews = await _context.News.OrderBy(n => n.DisplayOrder).ToListAsync();
             if (id == null)
             {
                 var firstType = await _context.TypeOfProduct.FirstAsync();
                 id = firstType.IdTypeOfProduct;
             }
-            ViewBag.ModelNews =
-                (
-                    from news in _context.News
-                    orderby news.DisplayOrder
-                    select news
-                ).ToList();
             return View(await _context.Product.Where(t => t.IdTypeOfProduct == id).ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewBag.TypeOfProducts = await _context.TypeOfProduct.ToListAsync();
+            ViewBag.ModelNews = await _context.News.OrderBy(n => n.DisplayOrder).ToListAsync();
+            var item = await _context.Product.FirstOrDefaultAsync(a => a.IdProduct == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
         }
     }
 }
