2e1d32bb89825d1eb25a588231585a079d13bceb
jtjaskulski@outlook.com <jtjaskulski@outlook.com>
2025-11-16 14:10:34 +0100

    Dodaje widok index z kontrollerem shop

---
+++ Company/Company.PortalWWW/Controllers/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using Company.Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.PortalWWW.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ILogger<ShopController> _logger;
+        private readonly CompanyContext _context;
+
+        public ShopController(CompanyContext context, ILogger<ShopController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            ViewBag.TypeOfProducts = await _context.TypeOfProduct.ToListAsync();
+            if (id == null)
+            {
+                var firstType = await _context.TypeOfProduct.FirstAsync();
+                id = firstType.IdTypeOfProduct;
+            }
+            return View(await _context.Product.Where(t => t.IdTypeOfProduct == id).ToListAsync());
+        }
+    }
+}
